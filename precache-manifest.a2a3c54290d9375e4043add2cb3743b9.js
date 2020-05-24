8f0854a9cd2c36bef4e41b28885c2720eb1007b9
elisaluan <elisaluan00@gmail.com>
2020-05-23 22:22:32 -0400

    publish

---
+++ precache-manifest.a2a3c54290d9375e4043add2cb3743b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495fee2daf236f8d43b729ec904dbebd",
+    "url": "/elisaluan.github.io/index.html"
+  },
+  {
+    "revision": "b40b7e1663790b1cc2d0",
+    "url": "/elisaluan.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "26d5814b922dcfe97677",
+    "url": "/elisaluan.github.io/static/js/2.f2856372.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elisaluan.github.io/static/js/2.f2856372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40b7e1663790b1cc2d0",
+    "url": "/elisaluan.github.io/static/js/main.2b35f3cf.chunk.js"
+  },
+  {
+    "revision": "4c8fb017bf68eb12430d",
+    "url": "/elisaluan.github.io/static/js/runtime-main.970cc2f7.js"
+  },
+  {
+    "revision": "34424cb4a9c03b72de3d38ed59634c6e",
+    "url": "/elisaluan.github.io/static/media/Gloss_And_Bloom.34424cb4.ttf"
+  }
+]);
